3dc8bf674993e94517d183802d8aab5315885633
summerorange7 <kapitan7016@icloud.com>
2022-02-07 22:20:16 +0900

    チャット画面の編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
- root to: "messages#index" #ルートパスはここへ行くよ:messages_controllerのindexアクション
+ root to: "rooms#index" #ルートパスはここへ行くよ:rooms_controllerのindexアクション
  resources :users, only: [:edit, :update]
  resources :rooms, only: [:new, :create]#roomsコントローラーへGo!・ただしnewとcreateの２アクションのみ
  
