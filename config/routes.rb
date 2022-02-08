a009b161c330ea9e6ec8c2177539391ca3f3e2c5
summerorange7 <kapitan7016@icloud.com>
2022-02-08 11:27:16 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
  root to: "rooms#index" #ルートパスはここへ行くよ:rooms_controllerのindexアクション
  resources :users, only: [:edit, :update]
- resources :rooms, only: [:new, :create]#roomsコントローラーへGo!・ただしnewとcreateの２アクションのみ
+ resources :rooms, only: [:new, :create] do#roomsコントローラーへGo!・ただしnewとcreateの２アクションのみ
+  resources :messages, only: [:index, :create]
+ end
+ 
  
 end
